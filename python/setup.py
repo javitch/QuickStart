3521ace01608906a127cf7d32cff6df53b853184
Christopher J. Hanks (Home) <cjhanks@cpusage.com>
2012-12-18 16:26:47 -0800

    Initialize the setup.py for Python

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+
+setup(name          = 'py-gridvid',
+      version       = '0.0.1',
+      description   = '',
+      author        = 'cjhanks@cpusage.com',
+      url           = 'https://gridvid.me',
+      packages      = ['gridvid'],
+      package_dir   = {'gridvid' : 'lib'} 
+      )
